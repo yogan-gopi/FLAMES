cc2d5af6a3819181281e5b3d4a7a4b6f4cbcd9b2
yogan-gopi <yogang2002@gmail.com>
2022-10-04 23:49:10 +0530

    OOPS update

--- SRC/app.py
+++ SRC/app.py
@@ -1,38 +1,47 @@
-def printRelation(f):
-    global name1, name2
+class Flames:   
+
     relations = {'F': "FRIENDS!", 'L': "LOVE!", 'A': "AFFECTION!", 'M': "MARRIAGE!", 'E': "ENEMY", 'S': "SISTERS!"}
-    prompt = "The Relationship between {} and {} will end in {}".format(name1, name2, relations[f])
-    print(prompt)
-    
-
-def calculateRelation(name1, name2):
     f  =['F', 'L', 'A', 'M', 'E', 'S']
-    name1 = name1.lower()
-    name2 = name2.lower()
-    name1.replace(" ", "")
-    name2.replace(" ", "")
+    
+    def __init__(self, name1 = "", name2 = ""):
 
-    for i in name1:
-        if i in name2:
-            name1 = name1.replace(i, "", 1)
-            name2 = name2.replace(i, "", 1)
+        if name1 == name2 == "":
+            self.name1 = input("Enter Your Name: ")
+            self.name2 = input("Enter Your Crush Name: ")
+        else:
+            self.name1 = name1
+            self.name2 = name2
+        
+        self.relation = self.calculateRelation()
+        self.printRelation()
 
-    n = len(name1) + len(name2)
-    i = 0
+    def printRelation(self): # To Display the relationship
 
-    for _ in range(5):
-        i -= 1
-        fLen = len(f)
-        for x in range(n):
-            i = ((i+1) % fLen)
-        f.pop(i)
-    return f[0]
+        self.prompt = "The Relationship between {} and {} will end in {}".format(self.name1 , self.name2, Flames.relations[self.relation])
+        print(self.prompt)
+        
+    def calculateRelation(self): # To find the relationship between name1 and name2
+        
+        n1 = self.name1.lower().rstrip().replace(" ", "")
+        n2 = self.name2.lower().rstrip().replace(" ", "")
+        
+        for i in n1:
+            if i in n2:
+                n1 = n1.replace(i, "", 1)
+                n2 = n2.replace(i, "", 1)
+
+        n = len(n1) + len(n2)
+        i = 0
+
+        for _ in range(5):
+            i -= 1
+            fLen = len(Flames.f)
+            for x in range(n):
+                i = ((i+1) % fLen)
+            Flames.f.pop(i)
+        return Flames.f[0]
     
 
 if __name__ == '__main__':
 
-    name1 = input("Enter Your Name: ")
-    name2 = input("Enter Your Crush Name: ")
-
-    relation = calculateRelation(name1, name2)
-    printRelation(relation)
+    Flames()
