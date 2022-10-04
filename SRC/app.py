2a7da19ca94d65127d5cdf85444bb9c2abca4c23
yogan-gopi <yogang2002@gmail.com>
2022-10-05 00:30:08 +0530

    OOPS update2

--- SRC/app.py
+++ SRC/app.py
@@ -5,7 +5,7 @@ class Flames:
     
     def __init__(self, name1 = "", name2 = ""):
 
-        if name1 == name2 == "":
+        if name1 == name2 == "": # Get names from user if not provided in the Constructor
             self.name1 = input("Enter Your Name: ")
             self.name2 = input("Enter Your Crush Name: ")
         else:
@@ -22,18 +22,18 @@ class Flames:
         
     def calculateRelation(self): # To find the relationship between name1 and name2
         
-        n1 = self.name1.lower().rstrip().replace(" ", "")
-        n2 = self.name2.lower().rstrip().replace(" ", "")
+        n1 = self.name1.lower().replace(" ", "") # Coverting to lowercase and removing the whitespace
+        n2 = self.name2.lower().replace(" ", "") # Coverting to lowercase and removing the whitespace
         
-        for i in n1:
+        for i in n1:                             # Removing the common characters present in name1 and name2
             if i in n2:
                 n1 = n1.replace(i, "", 1)
                 n2 = n2.replace(i, "", 1)
 
-        n = len(n1) + len(n2)
+        n = len(n1) + len(n2)                   
         i = 0
 
-        for _ in range(5):
+        for _ in range(5):                       # Finding the relation
             i -= 1
             fLen = len(Flames.f)
             for x in range(n):
